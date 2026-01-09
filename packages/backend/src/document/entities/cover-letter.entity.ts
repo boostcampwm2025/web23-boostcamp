3061b0d18606a8b1eaff76535c1bc0e2bd55011d
imjanghyeok <qkdrhkgn23@naver.com>
2026-01-09 10:02:33 +0900

    refactor: CoverLetter 엔티티 Import 스타일 개선 (#22)
    - CoverLetter 엔티티에서 Import 구문을 단일 라인 스타일로 변경
    - 코드를 보다 간결하고 일관되게 정리

--- packages/backend/src/document/entities/cover-letter.entity.ts
+++ packages/backend/src/document/entities/cover-letter.entity.ts
@@ -1,13 +1,6 @@
-import {
-    Entity,
-    PrimaryGeneratedColumn,
-    Column,
-    OneToOne,
-    JoinColumn,
-    OneToMany,
-} from 'typeorm';
-import { Document } from './document.entity';
-import { CoverLetterQuestionAnswer } from './cover-letter-question-answer.entity';
+import {Entity, JoinColumn, OneToMany, OneToOne, PrimaryGeneratedColumn,} from 'typeorm';
+import {Document} from './document.entity';
+import {CoverLetterQuestionAnswer} from './cover-letter-question-answer.entity';
 
 @Entity('cover_letters')
 export class CoverLetter {
