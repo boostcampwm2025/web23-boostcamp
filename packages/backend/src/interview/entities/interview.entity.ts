9405d6bab72ecbe9af5c36f05133fa8924da0c56
young999999999 <ssas146310@gmail.com>
2026-01-13 20:55:16 +0900

    feat: 면접 답변하기 기능 서비스 구현
    - 구현 편의를 위해 findExisting... 메서드를 만듦

--- packages/backend/src/interview/entities/interview.entity.ts
+++ packages/backend/src/interview/entities/interview.entity.ts
@@ -1,68 +1,77 @@
 import {
-    Entity,
-    PrimaryGeneratedColumn,
-    Column,
-    CreateDateColumn,
-    ManyToOne,
-    JoinColumn,
-    OneToMany,
-    OneToOne,
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  ManyToOne,
+  JoinColumn,
+  OneToMany,
+  OneToOne,
 } from 'typeorm';
 import { User } from '../../user/entities/user.entity';
 import { InterviewQuestion } from './interview-question.entity';
 import { InterviewAnswer } from './interview-answer.entity';
 import { LiveCodingInterview } from './live-coding-interview.entity';
 import { TechnicalInterview } from './technical-interview.entity';
+import { ForbiddenException } from '@nestjs/common';
 
 export enum InterviewType {
-    TECH = 'TECH',
-    CODE = 'CODE',
+  TECH = 'TECH',
+  CODE = 'CODE',
 }
 
 export enum LikeStatus {
-    NONE = 'NONE',
-    LIKE = 'LIKE',
-    DISLIKE = 'DISLIKE',
+  NONE = 'NONE',
+  LIKE = 'LIKE',
+  DISLIKE = 'DISLIKE',
 }
 
 @Entity('interviews')
 export class Interview {
-    @PrimaryGeneratedColumn({ type: 'bigint', name: 'interview_id' })
-    interviewId: string;
+  @PrimaryGeneratedColumn({ type: 'bigint', name: 'interview_id' })
+  interviewId: string;
 
-    @Column({ type: 'varchar', length: 255 })
-    title: string;
+  @Column({ type: 'varchar', length: 255 })
+  title: string;
 
-    @Column({ type: 'enum', enum: InterviewType })
-    type: InterviewType;
+  @Column({ type: 'enum', enum: InterviewType })
+  type: InterviewType;
 
-    @CreateDateColumn({ name: 'created_at' })
-    createdAt: Date;
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
 
-    @Column({
-        type: 'enum',
-        enum: LikeStatus,
-        name: 'like_status',
-        default: LikeStatus.NONE,
-    })
-    likeStatus: LikeStatus;
+  @Column({
+    type: 'enum',
+    enum: LikeStatus,
+    name: 'like_status',
+    default: LikeStatus.NONE,
+  })
+  likeStatus: LikeStatus;
 
-    @Column({ name: 'during_time', type: 'timestamp' })
-    duringTime: Date;
+  @Column({ name: 'during_time', type: 'timestamp' })
+  duringTime: Date;
 
-    @ManyToOne(() => User, (user) => user.interviews)
-    @JoinColumn({ name: 'user_id' })
-    user: User;
+  @ManyToOne(() => User, (user) => user.interviews)
+  @JoinColumn({ name: 'user_id' })
+  user: User;
 
-    @OneToMany(() => InterviewQuestion, (question) => question.interview)
-    questions: InterviewQuestion[];
+  @OneToMany(() => InterviewQuestion, (question) => question.interview)
+  questions: InterviewQuestion[];
 
-    @OneToMany(() => InterviewAnswer, (answer) => answer.interview)
-    answers: InterviewAnswer[];
+  @OneToMany(() => InterviewAnswer, (answer) => answer.interview, {
+    cascade: ['insert'],
+  })
+  answers: InterviewAnswer[];
 
-    @OneToOne(() => LiveCodingInterview, (live) => live.interview)
-    liveCoding: LiveCodingInterview;
+  @OneToOne(() => LiveCodingInterview, (live) => live.interview)
+  liveCoding: LiveCodingInterview;
 
-    @OneToOne(() => TechnicalInterview, (tech) => tech.interview)
-    technical: TechnicalInterview;
+  @OneToOne(() => TechnicalInterview, (tech) => tech.interview)
+  technical: TechnicalInterview;
+
+  validateUser(userId: string) {
+    if (this.user.userId !== userId) {
+      throw new ForbiddenException('인터뷰 소유자 이외의 접근입니다.');
+    }
+  }
 }
--- packages/backend/src/interview/interview.service.ts
+++ packages/backend/src/interview/interview.service.ts
@@ -1,4 +1,47 @@
 import { Injectable } from '@nestjs/common';
+import { InterviewRepository } from './interview.repository';
+import { Interview } from './entities/interview.entity';
+import { NotFoundException } from '@nestjs/common';
+import { SttService } from './stt.service';
+import { InterviewAnswer } from './entities/interview-answer.entity';
 
 @Injectable()
-export class InterviewService {}
+export class InterviewService {
+  constructor(
+    private readonly interviewRepository: InterviewRepository,
+    private readonly sttService: SttService,
+  ) {}
+
+  async answer(
+    userId: string,
+    interviewId: string,
+    file: Express.Multer.File,
+  ): Promise<string> {
+    const interview = await this.findExistingInterview(interviewId, [
+      'answers',
+      'user',
+    ]);
+    interview.validateUser(userId);
+    const sttResult = await this.sttService.transform(file);
+
+    // interview의 cascade를 활용해 answer 엔티티를 삽입한다.
+    const answer = new InterviewAnswer(sttResult);
+    interview.answers.push(answer);
+    await this.interviewRepository.save(interview);
+    return sttResult;
+  }
+
+  async findExistingInterview(
+    interviewId: string,
+    relations: string[] = [],
+  ): Promise<Interview> {
+    const interview = await this.interviewRepository.findById(
+      interviewId,
+      relations,
+    );
+    if (!interview) {
+      throw new NotFoundException('존재하지않는 인터뷰입니다.');
+    }
+    return interview;
+  }
+}
