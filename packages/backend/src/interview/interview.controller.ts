b69eb5ed284ea76f9fc285cfa75a5600ae8c261e
young999999999 <ssas146310@gmail.com>
2026-01-13 21:28:36 +0900

    feat: 인터뷰 답변하기 API 구현

--- packages/backend/src/interview/interview.controller.ts
+++ packages/backend/src/interview/interview.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Param, Post, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { UploadedFile } from '@nestjs/common';
+import { InterviewService } from './interview.service';
 
 @Controller('interview')
-export class InterviewController {}
+export class InterviewController {
+  constructor(private readonly interviewService: InterviewService) {}
+
+  @Post(':interviewId/answer')
+  @UseInterceptors(FileInterceptor('file'))
+  async answer(
+    @UploadedFile() file: Express.Multer.File,
+    @Param('interviewId') interviewId: string,
+  ): Promise<string> {
+    // 인증이 없기 때문에 userId를 상수화
+    const userId = '1';
+    return this.interviewService.answer(userId, interviewId, file);
+  }
+}
