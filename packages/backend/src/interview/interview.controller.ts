33cd31938025b809b64e5dd90a99b5122f26d8c8
Young999999999 <ssas146310@gmail.com>
2026-01-15 00:58:30 +0900

    [Refactor] 면접에서 사용자의  음성 답변 API 요청, 응답 dto 수정 (#34)
    * chore: 요청 DTO 검증을 위한 validator 의존성 추가
    
    * feat: 음성으로 답하기 요청, 응답 dto 설계
    
    * feat: 음성으로 답하기 API 에 dto 추가
    
    * style: 포매팅 변경

--- packages/backend/src/interview/interview.controller.ts
+++ packages/backend/src/interview/interview.controller.ts
@@ -1,20 +1,35 @@
-import { Controller, Param, Post, UseInterceptors } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { UploadedFile } from '@nestjs/common';
 import { InterviewService } from './interview.service';
+import { InterviewAnswerRequest } from './dto/interview-answer-request.dto';
+import { InterviewAnswerResponse } from './dto/interview-answer-response.dto';
 
 @Controller('interview')
 export class InterviewController {
-  constructor(private readonly interviewService: InterviewService) { }
+  constructor(private readonly interviewService: InterviewService) {}
 
-  @Post(':interviewId/answer/voice')
+  @Post('answer/voice')
   @UseInterceptors(FileInterceptor('file'))
   async answer(
     @UploadedFile() file: Express.Multer.File,
-    @Param('interviewId') interviewId: string,
-  ): Promise<string> {
+    @Body() body: InterviewAnswerRequest,
+  ): Promise<InterviewAnswerResponse> {
     // 인증이 없기 때문에 userId를 상수화
     const userId = '1';
-    return this.interviewService.answer(userId, interviewId, file);
+    const answerResult = await this.interviewService.answer(
+      userId,
+      body.interviewId,
+      file,
+    );
+
+    return {
+      answer: answerResult,
+    };
   }
 }
