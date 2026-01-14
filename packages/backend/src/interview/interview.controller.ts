8d48fb1a56c655c47f2990c46592cd30b674265c
young999999999 <ssas146310@gmail.com>
2026-01-14 19:10:43 +0900

    feat: 채팅으로 답변하기 API 구현

--- packages/backend/src/interview/interview.controller.ts
+++ packages/backend/src/interview/interview.controller.ts
@@ -7,25 +7,45 @@ import {
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { InterviewService } from './interview.service';
-import { InterviewAnswerRequest } from './dto/interview-answer-request.dto';
+import {
+  InterviewAnswerVoiceRequest,
+  InterviewAnswerChatRequest,
+} from './dto/interview-answer-request.dto';
 import { InterviewAnswerResponse } from './dto/interview-answer-response.dto';
 
 @Controller('interview')
 export class InterviewController {
-  constructor(private readonly interviewService: InterviewService) {}
+  constructor(private readonly interviewService: InterviewService) { }
 
   @Post('answer/voice')
   @UseInterceptors(FileInterceptor('file'))
-  async answer(
+  async answerWithVoice(
     @UploadedFile() file: Express.Multer.File,
-    @Body() body: InterviewAnswerRequest,
+    @Body() body: InterviewAnswerVoiceRequest,
   ): Promise<InterviewAnswerResponse> {
     // 인증이 없기 때문에 userId를 상수화
     const userId = '1';
-    const answerResult = await this.interviewService.answer(
+    const answerResult = await this.interviewService.answerWithVoice(
       userId,
       body.interviewId,
       file,
+    );
+
+    return {
+      answer: answerResult,
+    };
+  }
+
+  @Post('answer/chat')
+  async answerWithChat(
+    @Body() body: InterviewAnswerChatRequest,
+  ): Promise<InterviewAnswerResponse> {
+    // 인증이 없기 때문에 userId를 상수화
+    const userId = '1';
+    const answerResult = await this.interviewService.answerWithChat(
+      userId,
+      body.interviewId,
+      body.answer,
     );
 
     return {
