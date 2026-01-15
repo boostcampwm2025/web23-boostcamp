c3a8afb60c1ac5f7a6a2c9461311d3edb44052b8
imjanghyeok <qkdrhkgn23@naver.com>
2026-01-15 18:01:14 +0900

    feat: DocumentRepository 모듈에 추가 (#46)
    - `DocumentModule`에 DocumentRepository 등록
    - providers와 exports 항목에 DocumentRepository 추가

--- packages/backend/src/document/document.module.ts
+++ packages/backend/src/document/document.module.ts
@@ -3,10 +3,16 @@ import { DocumentController } from './document.controller';
 import { DocumentService } from './document.service';
 import { PortfolioRepository } from './repositories/portfolio.repository';
 import { CoverLetterRepository } from './repositories/cover-letter.repository';
+import { DocumentRepository } from './repositories/document.repository';
 
 @Module({
   controllers: [DocumentController],
-  providers: [DocumentService, PortfolioRepository, CoverLetterRepository],
-  exports: [PortfolioRepository, CoverLetterRepository],
+  providers: [
+    DocumentService,
+    PortfolioRepository,
+    CoverLetterRepository,
+    DocumentRepository,
+  ],
+  exports: [PortfolioRepository, CoverLetterRepository, DocumentRepository],
 })
 export class DocumentModule {}
