45c871cd5b08b2f9e38157187290a33736a8069c
imjanghyeok <qkdrhkgn23@naver.com>
2026-01-22 14:36:06 +0900

    feat: 포트폴리오 문서 생성 기능 추가 (#72)
    - `createPortfolioDocument` 함수 추가로 포트폴리오 문서 생성 로직 구현
    - DocumentType을 활용한 문서 유형 정의

--- packages/backend/src/document/repositories/document.repository.ts
+++ packages/backend/src/document/repositories/document.repository.ts
@@ -1,11 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { DataSource, In, Repository } from 'typeorm';
-import { Document } from '../entities/document.entity';
+import { Document, DocumentType } from '../entities/document.entity';
 
 @Injectable()
 export class DocumentRepository extends Repository<Document> {
   constructor(dataSource: DataSource) {
     super(Document, dataSource.createEntityManager());
+  }
+
+  async createPortfolioDocument(
+    title: string,
+    userId: string,
+  ): Promise<Document> {
+    return this.save({
+      type: DocumentType.PORTFOLIO,
+      title: title,
+      user: { userId },
+    });
   }
 
   async findByIds(ids: string[]): Promise<Document[]> {
