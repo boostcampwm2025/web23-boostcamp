a0c7cb94a4b77393e91c5cf797eca12a3ea99bfc
aaaa <71279997+kajj8808@users.noreply.github.com>
2026-01-29 21:33:34 +0900

    ✅ :update demo전 간단한 애매한 기능 수정 + 애매한 ui 부분 제거 or 수정 (#139)

--- packages/frontend/app/page.tsx
+++ packages/frontend/app/page.tsx
@@ -5,6 +5,7 @@ import { getUserSession } from "./lib/server/session";
 import StatusBadge from "./components/ui/status-badge";
 import { Button } from "./components/ui/button";
 import { ArrowRight } from "lucide-react";
+import GoogleLoginButton from "./(auth)/components/google-login-button";
 
 export default async function Home() {
   const userSession = await getUserSession();
@@ -19,7 +20,7 @@ export default async function Home() {
       <div className="flex size-full flex-col items-center justify-center gap-6 px-12 py-36">
         <StatusBadge text="새로운 AI 인터뷰 엔진" />
         <h1 className="text-center text-5xl font-bold text-pretty">
-          Recode your
+          Record your
           <br /> Interview.
         </h1>
         <p className="text-center text-lg text-pretty text-muted-foreground">
@@ -28,10 +29,9 @@ export default async function Home() {
           당신의 기술적 역량을 정밀하게 측정하고 개선하세요.
         </p>
         <div className="flex flex-col gap-4 sm:flex-row">
-          <Button>
-            시뮬레이션 시작하기 <ArrowRight />
-          </Button>
-          <Button variant="outline">Watch_demo</Button>
+          <GoogleLoginButton />
+          {/*           <Button variant="outline">Watch_demo</Button>
+           */}{" "}
         </div>
       </div>
     </div>
