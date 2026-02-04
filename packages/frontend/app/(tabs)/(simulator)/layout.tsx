091fd27fd46181e3481cbabfa77f62ba182f2290
kajj8808 <a01039388808@gmail.com>
2026-02-04 17:16:30 +0900

    🐛 update: 시뮬레이터 레이아웃 빌드 오류 수정

--- packages/frontend/app/(tabs)/(simulator)/layout.tsx
+++ packages/frontend/app/(tabs)/(simulator)/layout.tsx
@@ -1,7 +1,6 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-dvh flex-col">
-      <Header />
       <div className="flex-1">{children}</div>
     </div>
   );
